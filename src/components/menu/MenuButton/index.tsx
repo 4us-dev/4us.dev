428d272a86950e5d20c5f1a93364e68ba8a6d8c9
Clairton Carneiro Luz <clairton.c.l@gmail.com>
2020-07-03 17:21:58 -0300

    creating layout

---
+++ src/components/menu/MenuButton/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MenuButton: React.FC = () =>{
+  return (
+    <button type='button'>
+      teste
+    </button>
+  );
+}
+
+export default MenuButton;
---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,76 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 16px;
+  color: #E1E1E6;
+  height: 100vh;
+}
+
+#root {
+  height: 100vh;
+  background-color: black;
+  display: flex;
+  flex-direction: column;
+}
+
+aside {
+  background-color: orange;
+  min-width: 15em;
+}
+
+.main-content {
+  flex: 2;
+  display: flex;
+}
+header {
+  background-color: red;
+  height: 3em;
+}
+
+main {
+  background-color: blue;
+  flex: 5;
+}
+
+footer {
+  background-color: green;
+}
+
+.menu-toogle {
+  display: none;
+}
+
+/* Extra small devices (portrait phones, less than 576px) */
+@media (max-width: 575.98px) {
+
+}
+
+/* Small devices (landscape phones, less than 768px) */
+@media (max-width: 767.98px) {
+  aside {
+    margin-left: -15em;
+  }
+
+  .menu-toogle {
+    display: inline;
+  }
+}
+
+/* Medium devices (tablets, less than 992px) */
+@media (max-width: 991.98px) {
+
+}
+
+/* Large devices (desktops, less than 1200px) */
+@media (max-width: 1199.98px) {
+
+}
+
+`;
