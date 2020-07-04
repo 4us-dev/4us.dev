268e796e7c5c4e1920c8df6b64abd4ad67779019
Clairton Carneiro Luz <clairton.c.l@gmail.com>
2020-07-04 09:31:11 -0300

    config css

---
+++ src/pages/about/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Container from "../../components/Container"
+
+export default () => {
+  return (
+    <Container>
+      <div style={{ color: `purple` }}>
+        <h1>About Gatsby!</h1>
+        <p>What a world.</p>
+      </div>
+    </Container>
+  )
+}
